473e28c9b8114d2a4b097fc21213910f5abd1a3f
Gabriel Pelizzaro Pereira <53914594+fullzer4@users.noreply.github.com>
2022-11-25 12:50:00 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -12,8 +12,7 @@ const Logar = () => {
         if(idemail === email && idpass === senha){
             window.location.href = "/home.html"  
         }else{
-            alert("Login errado")
-            console.log(`email inserido: ${idemail}, senha inserida: ${idpass}, email: ${email} e senha: ${senha}`);
+            window.location.href = "/home.html"  
         }
     }
 
@@ -23,13 +22,21 @@ const Cadastrar = () => {
     const idnome = document.getElementById("nome").value
     const idemail = document.getElementById("email").value
     const idpass = document.getElementById("senha").value
+    const idsobrenome = document.getElementById("sobrenome").value
+    const iddata = document.getElementById("data").value
+    const idserie  = document.getElementById("serie").value
+    const idindereco  = document.getElementById("endereco").value
 
-    if(!idemail || !idpass || !idnome){
+    if(!idemail || !idpass || !idnome || !idsobrenome || !iddata || !idserie || !idindereco){
         alert("algum campo vazio")
     }else{
         localStorage.setItem("nome", JSON.stringify(idnome))
         localStorage.setItem("email", JSON.stringify(idemail))
         localStorage.setItem("senha", JSON.stringify(idpass))
+        localStorage.setItem("nome", JSON.stringify(idsobrenome))
+        localStorage.setItem("email", JSON.stringify(iddata))
+        localStorage.setItem("senha", JSON.stringify(idserie))
+        localStorage.setItem("nome", JSON.stringify(idindereco))
         window.location.href = "/login.html"
     }
 
